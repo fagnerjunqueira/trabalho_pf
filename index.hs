8e79a31e84de553752ce7aefc03bd3f7938a5306
MathiosBR <145026746+MathiosBR@users.noreply.github.com>
2023-09-14 17:51:48 -0300

    Update index.hs

--- index.hs
+++ index.hs
@@ -3,3 +3,11 @@ type Coordenadas = (Int, Int)
 type Estradas = [NomeCidade]
 type Cidade = (NomeCidade, Coordenadas, Estradas) --(String, (Int, Int),[String])
 type Mapa = [Cidade]
+
+--Função sem entrada
+mapa :: Mapa
+mapa = []
+
+--Adição de cidade ao mapa
+adcCidade :: Cidade -> Mapa -> Mapa
+adcCidade cidade0 mapa0 = cidade0:mapa0
