c682ea32afec6ba832db622ca3d4a11c177d2138
Fagner Junqueira <58519744+fagnerjunqueira@users.noreply.github.com>
2023-09-16 09:52:11 -0300

    Importação da biblioteca de mapa

--- index.hs
+++ index.hs
@@ -1,8 +1,4 @@
-type NomeCidade = String
-type Coordenadas = (Double, Double)
-type Estradas = [NomeCidade]
-type Cidade = (NomeCidade, Coordenadas, Estradas) --(String, (Int, Int),[String])
-type Mapa = [Cidade]
+import Mapa
 
 loadMapa :: Mapa
 loadMapa = [("Aracaju", (10.0,11.0), ["Recife", "Maceio"]), ("Maceio", (10.0,11.0), ["Recife", "Aracaju"]), ("Recife", (10.0,11.0), ["Aracaju", "Maceio"])]
@@ -18,23 +14,22 @@ rmvCidade :: Cidade -> Cidade
 rmvCidade cidade = cidade
 
 -- Adicionar estrada
-
-salvarEstrada :: Cidade -> Estradas -> Cidade
-salvarEstrada (cidade, coordenadas, estradas) novaEstrada
+salvarEstrada :: Cidade -> Rotas -> Cidade
+salvarEstrada (cidade, localizacao, rotas) novaEstrada
     | ps == [] = ("",(0,0),[""])
     |otherwise = head ps
-    where ps = [(cidade2, coordenadas2, estradas2++novaEstrada) | (cidade2, coordenadas2, estradas2)<-loadMapa, (cidade2 == cidade && coordenadas2 == coordenadas && estradas2 == estradas)]
+    where ps = [(cidade2, localizacao2, rotas2++novaEstrada) | (cidade2, localizacao2, rotas2)<-loadMapa, (cidade2 == cidade && localizacao2 == localizacao && rotas2 == rotas)]
 
-adcEstrada :: NomeCidade -> Estradas -> Cidade
-adcEstrada cidade estradas = salvarEstrada (snd (rodarMapa cidade mapa)) estradas
+adcEstrada :: Nome -> Rotas -> Cidade
+adcEstrada cidade rotas = salvarEstrada (snd (rodarMapa cidade mapa)) rotas
     where mapa = loadMapa
 
-rodarMapa :: NomeCidade -> Mapa -> (Bool, Cidade)
+rodarMapa :: Nome -> Mapa -> (Bool, Cidade)
 rodarMapa cidadeAlvo (x:xs)
     | fst (buscarCidade cidadeAlvo x) = (True, x)
     | otherwise = rodarMapa cidadeAlvo xs
 
-buscarCidade :: NomeCidade -> Cidade -> (Bool, Cidade)
-buscarCidade cidadeAlvo (cidade, coordenadas, estradas)
-    | cidadeAlvo == cidade = (True, (cidade, coordenadas, estradas))
-    | otherwise = (False, (cidade, coordenadas, estradas))
+buscarCidade :: Nome -> Cidade -> (Bool, Cidade)
+buscarCidade cidadeAlvo (cidade, localizacao, rotas)
+    | cidadeAlvo == cidade = (True, (cidade, localizacao, rotas))
+    | otherwise = (False, (cidade, localizacao, rotas))
