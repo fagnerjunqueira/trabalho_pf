d177134645ff1f0bf628fa84b258bd3efa64ccb0
Fagner Junqueira <58519744+fagnerjunqueira@users.noreply.github.com>
2023-09-16 11:02:53 -0300

    Teste com arquivo

--- index.hs
+++ index.hs
@@ -1,35 +1,35 @@
 import Mapa
+import Data.Typeable
+import Control.Monad
 
-loadMapa :: Mapa
-loadMapa = [("Aracaju", (10.0,11.0), ["Recife", "Maceio"]), ("Maceio", (10.0,11.0), ["Recife", "Aracaju"]), ("Recife", (10.0,11.0), ["Aracaju", "Maceio"])]
+main = do
+    mapaCompleto <- carregarMapa "teste.mapa"
+    print (mapaCompleto)
 
--- Função mapa vazio
-exibirMapa :: Mapa
-exibirMapa = []
+    -- Adicionar estrada
+    salvarEstrada :: Cidade -> Rotas -> Cidade
+    salvarEstrada (cidade, localizacao, rotas) novaEstrada
+        | ps == [] = ("",(0,0),[""])
+        | otherwise = head ps
+            where ps = [(cidade2, localizacao2, rotas2++novaEstrada) | (cidade2, localizacao2, rotas2)<-mapaCompleto, (cidade2 == cidade && localizacao2 == localizacao && rotas2 == rotas)]
 
-adcCidade :: Cidade -> Cidade
-adcCidade cidade = cidade
+    adcEstrada :: Nome -> Rotas -> Cidade
+    adcEstrada cidade rotas = salvarEstrada (snd (rodarMapa cidade mapa)) rotas
+        where mapa = mapaCompleto
 
-rmvCidade :: Cidade -> Cidade
-rmvCidade cidade = cidade
+    rodarMapa :: Nome -> IO Mapa -> (Bool, Cidade)
+    rodarMapa cidadeAlvo (x:xs)
+        | fst (buscarCidade cidadeAlvo x) = (True, x)
+        | otherwise = rodarMapa cidadeAlvo xs
 
--- Adicionar estrada
-salvarEstrada :: Cidade -> Rotas -> Cidade
-salvarEstrada (cidade, localizacao, rotas) novaEstrada
-    | ps == [] = ("",(0,0),[""])
-    |otherwise = head ps
-    where ps = [(cidade2, localizacao2, rotas2++novaEstrada) | (cidade2, localizacao2, rotas2)<-loadMapa, (cidade2 == cidade && localizacao2 == localizacao && rotas2 == rotas)]
+    buscarCidade :: Nome -> Cidade -> (Bool, Cidade)
+    buscarCidade cidadeAlvo (cidade, localizacao, rotas)
+        | cidadeAlvo == cidade = (True, (cidade, localizacao, rotas))
+        | otherwise = (False, (cidade, localizacao, rotas))
 
-adcEstrada :: Nome -> Rotas -> Cidade
-adcEstrada cidade rotas = salvarEstrada (snd (rodarMapa cidade mapa)) rotas
-    where mapa = loadMapa
+    -- loadMapa :: Mapa
+    -- loadMapa = mapaCompleto
+    -- [("Aracaju", (10.0,11.0), ["Recife", "Maceio"]), ("Maceio", (10.0,11.0), ["Recife", "Aracaju"]), ("Recife", (10.0,11.0), ["Aracaju", "Maceio"])]
 
-rodarMapa :: Nome -> Mapa -> (Bool, Cidade)
-rodarMapa cidadeAlvo (x:xs)
-    | fst (buscarCidade cidadeAlvo x) = (True, x)
-    | otherwise = rodarMapa cidadeAlvo xs
-
-buscarCidade :: Nome -> Cidade -> (Bool, Cidade)
-buscarCidade cidadeAlvo (cidade, localizacao, rotas)
-    | cidadeAlvo == cidade = (True, (cidade, localizacao, rotas))
-    | otherwise = (False, (cidade, localizacao, rotas))
+    rmvCidade :: Cidade -> Cidade
+    rmvCidade cidade = cidade
