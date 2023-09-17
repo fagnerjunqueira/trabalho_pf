6dae18e492b21fc9b7cdbd58d842fd274c625c07
Fagner Junqueira <58519744+fagnerjunqueira@users.noreply.github.com>
2023-09-17 17:52:52 -0300

    Função que remove cidade e a tira das rotas de outras cidades

--- index.hs
+++ index.hs
@@ -27,6 +27,20 @@ adcEstrada = do
 
     print novoMapa
 
+removido :: Rotas -> Nome -> Rotas
+removido [] _ = []
+removido (rotas:xs) estradaQuePerde
+    | rotas == [] = []
+    | rotas == estradaQuePerde = removido xs estradaQuePerde
+    | otherwise = removido xs estradaQuePerde ++ [rotas]
+
+tirarEstrada :: Mapa -> Nome -> Nome -> Mapa
+tirarEstrada [] _ _ = []
+tirarEstrada ((cidade, localizacao, rotas):xs) cidadeAlvo estradaQuePerde
+    | rotas == [] = []
+    | cidade == cidadeAlvo = [(cidade, localizacao, (removido rotas estradaQuePerde))] ++ tirarEstrada xs cidadeAlvo estradaQuePerde
+    | otherwise = [(cidade, localizacao, rotas)] ++ tirarEstrada xs cidadeAlvo estradaQuePerde
+
 rmvEstrada :: IO ()
 rmvEstrada = do
     putStrLn "Informe o nome do arquivo de mapa:"
@@ -36,22 +50,41 @@ rmvEstrada = do
     putStrLn "Digite a estrada que sera removida:"
     estradaQuePerde <- getLine
 
-    let removido :: Rotas -> Rotas
-        removido [] = []
-        removido (rotas:xs)
-            | rotas == [] = []
-            | rotas == estradaQuePerde = removido xs
-            | otherwise = removido xs ++ [rotas]
-
-    let tirarEstrada :: Mapa -> Mapa
-        tirarEstrada [] = []
-        tirarEstrada ((cidade, localizacao, rotas):xs)
-            | rotas == [] = []
-            | cidade == cidadeAlvo = [(cidade, localizacao, (removido rotas))] ++ tirarEstrada xs
-            | otherwise = [(cidade, localizacao, rotas)] ++ tirarEstrada xs
-
-    let novoMapa = tirarEstrada loadMapa 
+    let novoMapa = tirarEstrada loadMapa cidadeAlvo estradaQuePerde
 
     salvarMapa novoMapa "saida.mapa"
 
     print novoMapa
+
+rmvCidade :: IO ()
+rmvCidade = do
+    putStrLn "Informe o nome do arquivo de mapa:"
+    loadMapa <- carregarMapa "teste.mapa"
+    putStrLn "Informe a cidade que será removida:"
+    cidadeAlvo <- getLine
+
+    let remover :: Mapa -> Mapa
+        remover [] = []
+        remover ((cidade, localizacao, estradas):xs)
+            | [(cidade, localizacao, estradas)] == [] = []
+            | cidade == cidadeAlvo = remover xs
+            | otherwise = [(cidade, localizacao, estradas)] ++ remover xs
+    
+    let novoMapa = remover loadMapa 
+
+    let tirarEstrada :: Rotas -> Rotas
+        tirarEstrada [] = []
+        tirarEstrada (rotas:xs)
+            | rotas == [] = []
+            | rotas == cidadeAlvo = tirarEstrada xs
+            | otherwise = [rotas] ++ tirarEstrada xs
+
+    let mapaFinal :: Mapa -> Mapa
+        mapaFinal [] = []
+        mapaFinal ((cidade , coordenadas, rotas):xs)
+            | rotas == [] = []
+            | otherwise = [(cidade, coordenadas, (tirarEstrada rotas))] ++ mapaFinal xs
+
+    salvarMapa (mapaFinal novoMapa) "saida.mapa"
+
+    print (mapaFinal novoMapa)
